b03311269160e47e098d5098702544a3024394b4
Solanki Mahesh <Solanki777>
2025-10-18 23:39:15 +0530

    okk its done prac 24

--- prac24.py
+++ prac24.py
@@ -15,16 +15,34 @@
 
 
 # now using init function
-class car:
+# class car:
 
-    def __init__(self,fullname):
-        self.name=fullname
+#     def __init__(self,fullname):
+#         self.name=fullname
          
-        print("THIS IS NEW CAR ADDED BY TATA ")
+#         print("THIS IS NEW CAR ADDED BY TATA ")
 
 
-car1 = car("Maruti Suzuki")
-print(car1.name)
+# car1 = car("Maruti Suzuki")
+# print(car1.name)
 
-car2= car("Nano")
-print(car2.name)
+# car2= car("Nano")
+# print(car2.name)
+
+class student:
+    name=anonymmous  #class attribute
+    clgname="shantilal shah engineering college"
+    # non parameterized constroctor
+    def __init__(self):
+        pass
+    #  parameterized constructor
+    def __init__(self, name,marks):
+        self.name=name #object attribute which has higher priority than class attribute so it will print if some class attribute and object attribute name is same
+        self.marks=marks
+        print("addin new stdent data ...")
+
+s1=student("Mahesh",100)
+print(s1.name,s1.marks,s1.clgname)
+
+s2=student("Montu",100)
+print(s2.name,s2.marks)
