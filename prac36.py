6a5768a75e51819628c2176eb3912882d84b02d5
Solanki Mahesh <Solanki777>
2025-10-24 21:25:58 +0530

    getter and setter

--- prac36.py
+++ prac36.py
@@ -1,1 +1,25 @@
-getter and setters are methods used to access and update the attributes of a class. They provide controlled 
+# getter and setters are methods used to access and update the attributes of a class. They provide controlled 
+
+# class person:
+#     def __init__(self,date):
+#         # it is private data which can not access outside the class
+#         self.__birthdate=date
+
+# p1=person(2511)
+# print(p1.__birthdate) #now it will throw the error because of private variable
+
+
+
+class person:
+    def __init__(self,date):
+        self.__birthdate=date
+    # getter method
+    def get_birthdate(self):
+        return self.__birthdate
+
+p1=person(2511)
+# print(p1.__birthdate)
+
+# setter method
+data=p1.get_birthdate()
+print("data is given form setter method is",data)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-class student:
-    def __init__(self,phy,chem,math):
-        self.phy=phy
-        self.chem=chem
-        self.math=math
-        # self.percentage=str(( self.phy+self.math+self.chem)/3) + "%"
-    @property
-    def cal(self):
-        return str((self.phy+self.math+self.chem)/3)+"%"
-        
-s1=student(98,99,97)
-print(s1.cal)
-
-s1.phy=76
-print(s1.cal)
+print(p1.__birthdate)
