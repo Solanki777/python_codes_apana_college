3ad43343f3dfef9651fdffaebad34bd3c3d0e303
Solanki Mahesh <Solanki777>
2025-10-24 21:31:32 +0530

    polymorphism

---
+++ prac35.py
@@ -0,0 +1,8 @@
+# here its about polymorphism where it mean "having " diffrent forms as we can seen below example where we do a same operation but there is type is diffreent as python has some programmatic theory or algortihm that can deternmin the type and perform operation accordingly
+
+print(1+2) #3
+print(type(1))
+print("apana" + "college") #concatenate
+print(type("apana"))
+print([1,2,3]+[1,2,3]) #merge
+print(type([1,2,3]))
